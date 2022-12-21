65eb12b9c01dced37b54111fae3d573e557b0d4e
Manuel Luppino <60667104+Warlup@users.noreply.github.com>
2022-12-21 09:28:15 +0100

    javascript exercise

--- es-40-Local-Storage/exercise.js
+++ es-40-Local-Storage/exercise.js
@@ -2,4 +2,13 @@ const user = {
   id: 1,
   name: "John",
   age: 25,
-};
+}
+
+function store(user){
+  for (let key in user) {
+    localStorage.setItem(key, user[key])
+  }
+   
+}
+
+store(user)
--- es-41-Local-Storage/exercise.js
+++ es-41-Local-Storage/exercise.js
@@ -2,4 +2,18 @@ const user = {
   id: 1,
   name: "John",
   age: 25,
-};
+}
+
+function store(user){
+  for (let key in user) {
+    localStorage.setItem(key, user[key])
+  }
+}
+
+store(user)
+
+let data = localStorage.getItem("age")
+let data1 = localStorage.getItem("id")
+let data2 = localStorage.getItem("name")
+
+let arr = [data, data1, data2];
